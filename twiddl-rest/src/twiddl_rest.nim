b618e58628371df9a5954cfd5f9522a398cefbc8
joachimschmidt557 <joachim.schmidt557@outlook.com>
2019-06-08 13:01:44 +0200

    Remove python script, it's nim now

--- twiddl-rest/src/twiddl_rest.nim
+++ twiddl-rest/src/twiddl_rest.nim
@@ -1,7 +1,16 @@
 import asynchttpserver, asyncdispatch
+import osproc
+
+import apiv1
 
 proc requestHandler(req:Request) {.async.} =
-  await req.respond(Http200, "asdf")
+  if req.url.path == "/test":
+    await req.respond(Http200, "asdf")
+  if req.url.path == "/trigger-make":
+    await req.respond(Http200, "Triggered make")
+    discard execCmd("make")
+  else:
+    await req.respond(Http404, "Not a valid API call")
 
 when isMainModule:
   var server = newAsyncHttpServer()
